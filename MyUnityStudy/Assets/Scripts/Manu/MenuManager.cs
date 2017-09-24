cba376795651af9c52b8f23a19b9aff03a0487c7
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-09-24 19:39:26 +0900

    タイトルの追加

---
+++ MyUnityStudy/Assets/Scripts/Manu/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+	[SerializeField]SpriteRenderer TitleLogo;
+	[SerializeField]SpriteRenderer BackGround;
+	static bool showTitle = true;
+
+	void Awake(){
+		if (!showTitle) {
+			TitleLogo.sortingOrder = -3;
+			BackGround.sortingOrder = -2;
+		}
+			
+	}
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (showTitle && Input.anyKeyDown) {
+			TitleLogo.sortingOrder = -3;
+			BackGround.sortingOrder = -2;
+			showTitle = false;
+		}
+	}
+}
